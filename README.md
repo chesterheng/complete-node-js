2dca0d73d80a875bbecb80bd6b91a74d1e79b518
Chester Heng <chester.heng@gmail.com>
2020-06-13 13:54:14 +0800

    115. Authenticating Task Endpoints

--- README.md
+++ README.md
@@ -3069,7 +3069,70 @@ router.post('/tasks', auth, async (req, res) => {
 
 ### Authenticating Task Endpoints
 
+```javascript
+router.get('/tasks', auth, async (req, res) => {
+  try {
+    await req.user.populate('tasks').execPopulate()
+    res.send(req.user.tasks)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
 
+router.get('/tasks/:id', auth, async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id })
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+    
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    updates.forEach(update => task[update] = req.body[update])
+    await task.save()
+    res.send(task)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+    if (!task) {
+      res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+```
 
 **[⬆ back to top](#table-of-contents)**
 
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -12,37 +12,37 @@ router.post('/tasks', auth, async (req, res) => {
   try {
     await task.save()
     res.status(201).send(task)
-  } catch (e) {
-    res.status(400).send(e)
+  } catch (error) {
+    res.status(400).send(error)
   }
 })
 
 router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find({})
-    res.send(tasks)
-  } catch (e) {
+    await req.user.populate('tasks').execPopulate()
+    res.send(req.user.tasks)
+  } catch (error) {
     res.status(500).send()
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id
 
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id, owner: req.user._id })
 
     if (!task) {
       return res.status(404).send()
     }
 
     res.send(task)
-  } catch (e) {
+  } catch (error) {
     res.status(500).send()
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -52,31 +52,30 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id)
-    updates.forEach((update) => task[update] = req.body[update])
-    await task.save()
-    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+    
     if (!task) {
-        return res.status(404).send()
+      return res.status(404).send()
     }
 
+    updates.forEach(update => task[update] = req.body[update])
+    await task.save()
     res.send(task)
-  } catch (e) {
-    res.status(400).send(e)
+  } catch (error) {
+    res.status(400).send(error)
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id)
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
     if (!task) {
       res.status(404).send()
     }
 
     res.send(task)
-  } catch (e) {
+  } catch (error) {
     res.status(500).send()
   }
 })
