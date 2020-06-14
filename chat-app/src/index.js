e853d8e7af47d7aeba4358a1600f0191cb88d028
Chester Heng <chester.heng@gmail.com>
2020-06-14 14:11:09 +0800

    172. Rendering User List

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -31,6 +31,11 @@ io.on('connection', socket => {
 
     socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    })
+
     callback()
 
   })
@@ -56,6 +61,11 @@ io.on('connection', socket => {
     const user = removeUser(socket.id)
     if (user) {
       io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+      
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      })
     }
   })
 })
